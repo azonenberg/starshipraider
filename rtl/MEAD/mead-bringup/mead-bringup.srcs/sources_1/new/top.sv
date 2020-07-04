32bf9fb74ea0930393cd058ea592596ec54403a7
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2020-07-03 23:33:14 -0700

    Initial FPGA design for MEAD bringup

---
+++ rtl/MEAD/mead-bringup/mead-bringup.srcs/sources_1/new/top.sv
@@ -0,0 +1,101 @@
+`timescale 1ns/1ps
+`default_nettype none
+
+module top(
+	input wire 			clk_25mhz,
+
+	input wire[7:0]		probe_p,
+	input wire[7:0]		probe_n,
+
+	inout wire			uart_tx,
+	input wire			uart_rx,
+
+	input wire			presence_detect,
+	output logic		power_en	= 0,
+
+	output logic[3:0]	led			= 0
+);
+
+	assign uart_tx = 1'bz;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Input buffers
+
+	wire[7:0]	probe;
+	DifferentialInputBuffer #(
+		.WIDTH(8),
+		.IOSTANDARD("LVDS_25"),
+		.ODT(1),
+		.OPTIMIZE("SPEED")
+	) probe_ibuf (
+		.pad_in_p(probe_p),
+		.pad_in_n(probe_n),
+		.fabric_out(probe)
+	);
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Synchronize async inputs
+
+	wire	present_n;
+
+	ThreeStageSynchronizer #(
+		.INIT(0),
+		.IN_REG(0)
+	) sync_present(
+		.clk_in(clk_25mhz),
+		.din(presence_detect),
+		.clk_out(clk_25mhz),
+		.dout(present_n)
+	);
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Probe power hotswap
+
+	//Wait about 670ms after mate before applying power
+	logic[22:0] count = 0;
+
+	enum logic[1:0]
+	{
+		STATE_OFF,
+		STATE_TURNING_ON,
+		STATE_ON
+	} state = STATE_OFF;
+
+	always_ff @(posedge clk_25mhz) begin
+
+		case(state)
+
+			STATE_OFF: begin
+
+				if(!present_n) begin
+					led[0]	<= 1;
+					count	<= 1;
+					state	<= STATE_TURNING_ON;
+				end
+
+			end	//end STATE_OFF
+
+			STATE_TURNING_ON: begin
+				count	<= count + 1;
+
+				if(count == 0) begin
+					led[1]		<= 1;
+					power_en	<= 1;
+					state		<= STATE_ON;
+				end
+
+			end	//end STATE_TURNING_ON
+
+			STATE_ON: begin
+				if(present_n) begin
+					led			<= 0;
+					power_en	<= 0;
+					state		<= STATE_OFF;
+				end
+			end	//end STATE_ON
+
+		endcase
+
+	end
+
+endmodule
